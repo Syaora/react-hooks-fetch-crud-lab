fb222592fe616192b69eca400f9bbeaafe30ab14
Elizabeth Nguyen <saper209@yahoo.com>
2022-06-12 14:23:10 -0700

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((question) => setQuestions(question))
+  }, [])
+
+  function handleUpdateQuestion(updatedQuestion){
+    const updatedQuestions = questions.map((question) => {
+      if (question.id === updatedQuestion.id){
+        return updatedQuestion
+      } else {
+        return question
+      }
+    })
+  }
+
+  function handleAddQuestion(newQuestion){
+    setQuestions([...questions, newQuestion])
+  }
+
+  function handleDeleteQuestion(deletedQuestion){
+    const updatedQuestions = questions.filter((question) => question.id !== deletedQuestion.id)
+    setQuestions(updatedQuestions)
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddQuestion={handleAddQuestion} /> : <QuestionList questions={questions} onUpdateQuestion={handleUpdateQuestion} onDeleteQuestion={handleDeleteQuestion} />}
     </main>
   );
 }
